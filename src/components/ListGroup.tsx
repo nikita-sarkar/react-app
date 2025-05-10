8e31a9fe7da6f4decf8bda8c925049b63092a967
nikita-sarkar <nikita2019sarkar@gmail.com>
2025-05-10 19:40:16 +0530

    Using fragments and dynamically passing values

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,20 @@
+
 function ListGroup() {
-  return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+  
+    const items = [
+        'Apples',
+        'Bananas',
+        'Grapes'
+    ];
+  
+    return (
+    <>
+        <h1>List</h1>
+        <ul className="list-group">
+        {items.map(item => 
+            <li key={item}>{item}</li>)}
+        </ul>
+    </>
   );
 }
 
