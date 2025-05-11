b782d7efa8e69d4010eb390e24659e570159dc49
nikita-sarkar <nikita2019sarkar@gmail.com>
2025-05-11 21:06:33 +0530

    Conditional formatting

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,21 @@
 
 function ListGroup() {
   
-    const items = [
+    let items = [
         'Apples',
         'Bananas',
         'Grapes'
     ];
-  
+    items = [];
+
+    // const getMessage = () => {
+    //    return items.length === 0 ? <p>No item found</p> : null;
+    // }
+
     return (
     <>
         <h1>List</h1>
+        {items.length === 0 && <p>No item found</p>}
         <ul className="list-group">
         {items.map(item => 
             <li key={item}>{item}</li>)}
